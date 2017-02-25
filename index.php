d52622f0e48cb76fd3e1cb92728ca9c7b8e984b1
Bianca95 <b.prodan95@gmail.com>
2017-02-25 14:12:00 +0000

    First action

--- index.php
+++ index.php
@@ -7,4 +7,9 @@ $client = new SlackRealTimeClient($loop);
 $client->setToken('xoxb-147136165447-oXQ85V092xXMZwtf5NxjN1Cs');
 $client->connect();
 
+$client->on('message', function ($data) use ($client) {
+    echo "Incoming message: ".$data['text']."\n";
+});
+ 
+$loop->run();
 ?>
